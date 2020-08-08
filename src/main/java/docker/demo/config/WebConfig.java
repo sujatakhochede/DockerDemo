7c19a9230c359b03b748ce594b6deaffed14adfc
nandlaldhande <44774126+nandlaldhande@users.noreply.github.com>
2020-08-08 15:22:00 +0200

    Add Demo project

---
+++ src/main/java/docker/demo/config/WebConfig.java
@@ -0,0 +1,11 @@
+package docker.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.docker.demo")
+public class WebConfig {
+}
